1690cc039cc702bbbe04c1de56b0379af78d71ed
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-02-21 18:13:10 +0100

    Default pawn color

--- src/main/java/chess/Pawn.java
+++ src/main/java/chess/Pawn.java
@@ -1,5 +1,11 @@
 class Pawn {
+    static final Color DEFAULT_COLOR = Color.WHITE;
+
     private Color color;
+
+    Pawn() {
+	this(DEFAULT_COLOR);
+    }
     
     Pawn(Color color) {
 	this.color = color;
--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -1,9 +1,15 @@
 import junit.framework.TestCase;
 
 public class PawnTest extends TestCase {
-    public void testCreate() {
+    public void testCreateSpecifyingColor() {
 	final Pawn pawn = new Pawn(Color.BLACK);
 	
 	assertEquals(Color.BLACK, pawn.getColor());
+    }
+
+    public void testCreateDefaultColor() {
+	final Pawn pawn = new Pawn();
+
+	assertEquals(Pawn.DEFAULT_COLOR, pawn.getColor());
     }
 }
